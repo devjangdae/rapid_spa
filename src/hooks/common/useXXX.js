193bc19f8eb44aaf3da7cfb8e0ba8ac4c327a2d4
devjangdae <tjqkd0912@gmail.com>
2023-02-11 21:26:03 +0900

    그 외 폴더들 추가

---
+++ src/hooks/common/useXXX.js
@@ -0,0 +1,2 @@
+// 각종 컴포넌트에서 공용으로 사용할 customHook의 집합
+// 파일명은 useXxx.ts(jsx)의 네이밍 규칙을 따를것!(camel case)
---
+++ src/hooks/query/queryKeyXXX.js
@@ -0,0 +1,3 @@
+// react-query에서 사용하는 함수의 집합
+// 1. 도메인 단위로 페이지를 작성할것!
+// 2. 파일내 함수는 use[Get/Post/Delete/Put][도메인명]Xxx.ts(jsx)의 네이밍 규칙을 따를것!(camel case)
