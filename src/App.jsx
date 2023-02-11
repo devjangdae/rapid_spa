d8b74bd63ea74467071835df8c492354691525b2
devjangdae <tjqkd0912@gmail.com>
2023-02-11 22:33:00 +0900

    로그인 페이지 컴포넌트 분리 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
-import Loginrapid2 from "./pages/loginrapid/Loginrapid2";
+import Loginrapid from "./pages/loginrapid/Loginrapid";
 import Mainrapid from "./pages/mainrapid/Mainrapid";
 import Maintest from "./pages/maintest/Maintest";
 
 function App() {
-  console.log("asd");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Loginrapid2 />} />
+        <Route path="/" element={<Loginrapid />} />
         <Route path="/mainrapid" element={<Mainrapid />} />
         <Route path="/maintest" element={<Maintest />} />
       </Routes>
