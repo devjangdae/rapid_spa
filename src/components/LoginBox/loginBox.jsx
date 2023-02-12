9b89a9b0d839f6984f6b5d02be0a64fcab9a43a6
devjangdae <tjqkd0912@gmail.com>
2023-02-12 22:32:47 +0900

    로그인페이지 구현중

--- src/components/LoginBox/loginBox.jsx
+++ src/components/LoginBox/loginBox.jsx
@@ -1,6 +1,9 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from "react";
 import { css } from "@emotion/react";
-import { Input, Button } from "antd";
+import { Form, Input, Button, message } from "antd";
+import axios from "axios";
+import * as md5 from "md5";
 
 const loginTextWrap = css`
   display: flex;
@@ -10,7 +13,7 @@ const loginWrap = css`
   display: flex;
   flex-direction: column;
   width: 450px;
-  gap: 20px;
+  gap: 15px;
   padding: 50px;
 `;
 
@@ -29,27 +32,86 @@ const loginBox = css`
 `;
 
 function LoginBox() {
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = () => {
+    // Prevent the default submit and page reload
+    // e.preventDefault();
+    message.error("Fail");
+
+    // Handle validations
+    axios
+      .post(`/rss/api/auths/login?username=${id}&password=${md5(password)}`)
+      .then((response) => {
+        console.log(response);
+
+        // Handle response
+        message.success("Login Suceessful!");
+      });
+  };
+
   return (
     <div className="loginBox" css={[loginBox]}>
-      <div className="loginWrap" css={[loginWrap]}>
-        <div>
+      <Form onFinish={handleSubmit}>
+        <div className="loginWrap" css={[loginWrap]}>
           <div className="loginTextWrap" css={[loginTextWrap]}>
-            id
+            ID
           </div>
           <div>
-            <Input placeholder="Basic usage" />
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  type: "string",
+                  message: "Please enter id",
+                },
+              ]}
+              name="userid"
+            >
+              <Input
+                placeholder="Enter your id"
+                type="id"
+                name="id"
+                id="id"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+            </Form.Item>
           </div>
-        </div>
-        <div>
           <div className="loginTextWrap" css={[loginTextWrap]}>
-            pw
+            PASSWORD
           </div>
           <div>
-            <Input placeholder="Basic usage" />
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter password",
+                },
+              ]}
+              name="userpassword"
+            >
+              <Input.Password
+                placeholder="Enter your password"
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Item>
           </div>
+          <Button
+            style={{ height: "80px", fontSize: "32px" }}
+            type="primary"
+            htmlType="submit"
+            block
+          >
+            Login
+          </Button>
         </div>
-        <Button type="primary">Primary Button</Button>
-      </div>
+      </Form>
     </div>
   );
 }
