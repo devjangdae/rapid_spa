ad7bf8aeeccdfc3fea607e1948a8918531c5aa47
devjangdae <tjqkd0912@gmail.com>
2023-02-20 16:24:42 +0900

    manual 페이지 컴포넌트 분리

---
+++ src/components/Manual/ManualBox.jsx
@@ -0,0 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import SelectBox from "../SelectBox/index";
+import DataEmpty from "../DataEmpty/index";
+import DataTable from "../DataTable/index";
+
+const contentWrap = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const selectBoxWrap = css`
+  display: flex;
+  flex-direction: column;
+  width: 1440px;
+`;
+
+const dataList = 0;
+// 임시
+
+function ManualBox() {
+  return (
+    <div className="contentWrap" css={contentWrap}>
+      <div className="row01" css={selectBoxWrap}>
+        <SelectBox />
+      </div>
+      <div className="row02">
+        {dataList === 1 ? <DataTable /> : <DataEmpty />}
+      </div>
+    </div>
+  );
+}
+
+export default ManualBox;
