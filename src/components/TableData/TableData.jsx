f644107e756c5527f34182c2186d9592bc1d8d6f
devjangdae <tjqkd0912@gmail.com>
2023-02-21 16:10:42 +0900

    테이블 컴포넌트 분리 코드 정리

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -7,64 +7,6 @@ import { useEffect, useState } from "react";
 // import { dataSource } from "../../constants/dataSource";
 import { columns } from "../../constants/columns";
 
-const columns2 = [
-  {
-    title: "Machine",
-    dataIndex: "gender",
-    filters: [
-      {
-        text: "Male",
-        value: "male",
-      },
-      {
-        text: "Female",
-        value: "female",
-      },
-    ],
-    width: "20%",
-  },
-  {
-    title: "Category",
-    dataIndex: "gender",
-    filters: [
-      {
-        text: "Male",
-        value: "male",
-      },
-      {
-        text: "Female",
-        value: "female",
-      },
-    ],
-    width: "20%",
-  },
-  {
-    title: "File Name",
-    dataIndex: "name",
-    sorter: true,
-    render: (name) => `${name.first} ${name.last}`,
-    width: "20%",
-  },
-  {
-    title: "Date",
-    dataIndex: "name",
-    sorter: true,
-    render: (name) => `${name.first} ${name.last}`,
-    width: "20%",
-  },
-  {
-    title: "Size",
-    dataIndex: "name",
-    sorter: true,
-    render: (name) => `${name.first} ${name.last}`,
-    width: "20%",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-  },
-];
-
 const tableStyle = css`
   table-layout: auto;
   width: 100%;
@@ -78,65 +20,9 @@ const getRandomuserParams = (params) => ({
 
 function TableData() {
   const accessToken = sessionStorage.getItem("accessToken");
-  // console.log(`엑세스토큰${accessToken}`);
-
-  const [machineList, setMachineList] = useState([]);
-  const [uniqueLine, setUniqueLine] = useState([]);
-  const [categoryList, setCategoryList] = useState([]);
   const [finalListData, setFinalListData] = useState([]);
 
   useEffect(() => {
-    // MACHINE
-    const fetchMachine = async () => {
-      try {
-        const response = await axios.get("/rss/api/system/machinesInfo/", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const arrayUniqueByLine = [
-          ...new Map(
-            response.data.lists.map((item) => [item.line, item])
-          ).values(),
-        ];
-
-        setUniqueLine(arrayUniqueByLine);
-        setMachineList(response.data.lists);
-        // console.log(`machineList${machineList}`);
-        // console.log(`uniqueLine${uniqueLine}`);
-      } catch (e) {
-        // console.log(e);
-      }
-    };
-
-    fetchMachine();
-
-    // CATEGORY
-    const fetchCategory = async () => {
-      try {
-        const response = await axios.get("/rss/api/system/categoryInfo/", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const tempList = [];
-        for (let i = 0; i < response.data.lists.length; i += 1) {
-          tempList.push(
-            `${response.data.lists[i].categoryCode}_${response.data.lists[i].categoryName}`
-          );
-        }
-
-        setCategoryList(tempList);
-        // console.log(`tempList${tempList}`);
-      } catch (e) {
-        // console.log(e);
-      }
-    };
-
-    fetchCategory();
-
     // SEARCH URL
     let searchId2;
     let resultId2;
@@ -159,11 +45,7 @@ function TableData() {
             },
           }
         );
-
-        // console.log(response.data.searchId);
         searchId2 = response.data.searchId;
-        // setSearchId3(response.data.searchId);
-
         try {
           const response = await axios.get(`/rss/api/ftp/search/${searchId2}`, {
             params: { searchId: searchId2 },
@@ -171,10 +53,7 @@ function TableData() {
               Authorization: `Bearer ${accessToken}`,
             },
           });
-
-          // console.log(response);
           resultId2 = response.data.resultUrl;
-
           try {
             const response = await axios.get(`${resultId2}`, {
               params: {
@@ -184,13 +63,11 @@ function TableData() {
                 Authorization: `Bearer ${accessToken}`,
               },
             });
+            // fianlListData 최종데이터(api -> 서치 완성하면 store로 변경필요함.)
             setFinalListData(response.data.lists);
-            console.log("@f@");
-            console.log(finalListData);
             // /rss/api/ftp/search/result/SFTP-20230221135513619-5137
           } catch (e) {
-            console.log(e);
-            console.log("@e@");
+            // console.log(e);
           }
         } catch (e) {
           // console.log(e);
@@ -199,35 +76,9 @@ function TableData() {
         // console.log(e);
       }
     };
-
     fetchSearchURL();
     // SFTP-20230221135043558-5127
   }, []);
-
-  // useEffect(() => {
-  //   const loadData = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         `/rss/api/ftp/search/result/SFTP-20230221141712552-5171`,
-  //         {
-  //           params: {
-  //             resultUrl:
-  //               "/rss/api/ftp/search/result/SFTP-20230221141712552-5171",
-  //           },
-  //           headers: {
-  //             Authorization: `Bearer ${accessToken}`,
-  //           },
-  //         }
-  //       );
-  //       finalListData = response.data.lists;
-  //       console.log(finalListData);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-
-  //   loadData();
-  // }, []);
 
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
@@ -239,8 +90,8 @@ function TableData() {
   });
 
   const timeoutId = setTimeout(function () {
-    console.log("Hello World");
-  }, 2000);
+    console.log("api호출중 setTimeOut..");
+  }, 1000);
 
   const fetchData = () => {
     clearTimeout(timeoutId);
@@ -256,7 +107,6 @@ function TableData() {
         setData(results);
         // console.log(tableParams);
         // console.log(data);
-
         setLoading(false);
         setTableParams({
           ...tableParams,
@@ -265,6 +115,7 @@ function TableData() {
             total: 200,
             // 200 is mock data, you should read it from server
             // total: data.totalCount,
+            // 나중에 토탈 값 넣어줘야 함, sort도
           },
         });
       });
@@ -291,7 +142,6 @@ function TableData() {
     <Table
       css={tableStyle}
       columns={columns}
-      // rowKey={(record) => record.login.uuid}
       dataSource={finalListData}
       pagination={tableParams.pagination}
       loading={loading}
