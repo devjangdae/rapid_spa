9a1f736805b092300d266b407a34ea55817f1c05
parksy1001 <gracefirst24@naver.com>
2023-02-22 13:10:43 +0900

    current 상태 추가
    current 상태 추가

--- src/reducers/slices/mainSlice.js
+++ src/reducers/slices/mainSlice.js
@@ -2,7 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const mainSlice = createSlice({
   name: "main",
-  initialState: { isSearching: false, isOpened: true },
+  initialState: {
+    isSearching: false,
+    isOpened: true,
+    currentDate: "",
+    currentStartDate: "",
+    currentEndDate: "",
+    currentCategory: [],
+    currentCategoryCode: [],
+    currentCategoryName: [],
+    currentFabMachineName: [],
+    currentFabName: [],
+    currentMachineName: [],
+  },
   reducers: {
     clickSearchButton: (state, action) => {
       state.isSearching = true;
@@ -17,9 +29,49 @@ const mainSlice = createSlice({
     closeDrawer: (state, action) => {
       state.isOpened = false;
     },
+    currentDateUpdate: (state, action) => {
+      state.currentDate = action.payload;
+    },
+    currentStartDateUpdate: (state, action) => {
+      state.currentStartDate = action.payload;
+    },
+    currentEndDateUpdate: (state, action) => {
+      state.currentEndDate = action.payload;
+    },
+    currentCategoryUpdate: (state, action) => {
+      state.currentCategory.push(action.payload);
+    },
+    currentCategoryCodeUpdate: (state, action) => {
+      state.currentCategoryCode.push(action.payload);
+    },
+    currentCategoryNameUpdate: (state, action) => {
+      state.currentCategoryName.push(action.payload);
+    },
+    currentFabMachineNameUpdate: (state, action) => {
+      state.currentFabMachineName.push(action.payload);
+    },
+    currentMachineNameUpdate: (state, action) => {
+      state.currentMachineName.push(action.payload);
+    },
+    currentFabNameUpdate: (state, action) => {
+      state.currentFabName.push(action.payload);
+    },
   },
 });
 
 export default mainSlice;
-export const { clickResetButton, clickSearchButton, openDrawer, closeDrawer } =
-  mainSlice.actions;
+export const {
+  clickResetButton,
+  clickSearchButton,
+  openDrawer,
+  closeDrawer,
+  currentDateUpdate,
+  currentStartDateUpdate,
+  currentEndDateUpdate,
+  currentCategoryUpdate,
+  currentCategoryCodeUpdate,
+  currentCategoryNameUpdate,
+  currentFabMachineNameUpdate,
+  currentMachineNameUpdate,
+  currentFabNameUpdate,
+} = mainSlice.actions;
